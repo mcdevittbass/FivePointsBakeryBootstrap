8601ab2abf9814264dd7756ef8874a8543f0c1a2
Megan McD <mcdevittbass@gmail.com>
2020-02-29 07:36:39 -0500

    Contact, Ecent Space, Menu. Navbar still not fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 $(function() {
-    $("#IGBar").mouseover(function(){
+    $("#IGCard").mouseover(function(){
         $("#IGBar").css("opacity", "0.5");
         $("#IGOverlay").removeClass("d-none");
     });
-        $("#IGOverlay").mouseout(function(){
+        $("#IGCard").mouseout(function(){
         $("#IGBar").css("opacity", "1");
         $("#IGOverlay").addClass("d-none");
     });
-    
+});
+
+$(function(){
+    $(".carousel").carousel("cycle");
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
+
+$(function() {
+    $("#requestEvent").click(function(){
+        $("#eventModal").modal("show");
+    });
 });
